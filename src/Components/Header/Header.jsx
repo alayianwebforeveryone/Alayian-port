ba985831a39114e836edaaaf8e0a690328c5a96c
unknown <alayianwebforeveryone>
2024-01-08 08:35:37 -0800

    responsive improvements

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
         if (icon === "menu-outline") {
 
             setIcon("close-outline")
-            setTopSpace("top-[15%] left-[25%] bg-[#cb8be4] pb-2 w-[35%]")
+            setTopSpace("top-[10%] left-[0%] bg-[#cb8be4]  pb-2 w-[100%]   ")
         }
         else {
             setTopSpace("top-[-1000%]")
@@ -26,7 +26,6 @@ function Header() {
         <div className="md:flex    md:flex-row  flex justify-between    sm:justify-around text-white md:w-[100%] mx-auto w-[100%] 
         items-center py-5 h-auto  px-6 sm:px-0"
             style={{
-                // borderBottom: "1px solid gray",
                 boxShadow: "2px 2px 4px 2px rgba(0, 0, 0, 0.1)",
                 background: "linear-gradient(90deg, rgba(113,8,125,1) 0%, rgba(93,176,219,1) 56%)"
             }}
@@ -39,53 +38,49 @@ function Header() {
             >
                 <ion-icon name={icon} className=" cursor-pointer burger "></ion-icon>
             </div>
-            <div className={`${topSpace}  left-[5%] px-5  mt-4  sm:mt-0 absolute sm:static   `}>
-                <ul className=" flex sm:flex-row flex-col sm:items-center sm:gap-16 gap-3 :sm-center">
-                    <motion.li
+            <div className={`${topSpace}   px-5  mt-4  sm:mt-0 absolute sm:static   `}>
+                <ul className="  flex sm:flex-row flex-col sm:items-center w-20 md:w-auto sm:gap-16 gap-3 :sm-center">
+                    <motion.li 
                         whileHover={{
-                            borderBottom: "2px solid green",
-                            transition: { duration: 2 }
+                           
                         }}
-                        className={({ isActive }) => `hover:text-[#2675b6] ${isActive ? "text-blue" : "text-black"}`}
+                        className="  hover:text-purple-700"
                     >
                         <NavLink to="/">Home</NavLink>
                     </motion.li>
                     <motion.li
                         whileHover={{
-                            borderBottom: "2px solid green",
-                            transition: { duration: 2 }
+                           
+                           
                         }}
-                        className={({ isActive }) => `hover:text-[#2675b6] ${isActive ? "text-blue" : "text-black"}`}>
+                        className="  hover:text-purple-700">
                         <NavLink to="/services">
                             Services
                         </NavLink>
                     </motion.li>
                     <motion.li
                         whileHover={{
-                            borderBottom: "2px solid green",
-                            transition: { duration: 2 }
+                            
                         }}
-                        className={({ isActive }) => `hover:text-[#2675b6] ${isActive ? "text-blue" : "text-black"}`}>
+                        className=" border-1  hover:text-purple-700">
                         <NavLink to="/portfolio">
                             Portfolio
                         </NavLink>
                     </motion.li>
                     <motion.li
                         whileHover={{
-                            borderBottom: "2px solid green",
-                            transition: { duration: 2 }
+                           
                         }}
-                        className={({ isActive }) => `hover:text-[#2675b6] ${isActive ? "text-blue" : "text-black"}`}>
+                        className="  hover:text-purple-700">
                         <NavLink to="/contact">
                             Contact
                         </NavLink>
                     </motion.li>
                     <motion.li
                         whileHover={{
-                            borderBottom: "2px solid green",
-                            transition: { duration: 2 }
+                           
                         }}
-                        className={({ isActive }) => `hover:text-[#2675b6] ${isActive ? "text-blue" : "text-black"}`}>
+                        className="  hover:text-purple-700">
                         <NavLink to="/about">
                             About
                         </NavLink>
